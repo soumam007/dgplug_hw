35f8f2d615fb475f98b4680fe6f65d662accca5e
Kshitij <kshtjkmr35@gmail.com>
2017-08-08 21:14:15 +0530

    changes according to PEP8 guidlines

--- bmi.py
+++ bmi.py
@@ -1,46 +1,41 @@
-#this is a short python program to calculate the bmi of a person
-#home-work of dgplug
+# This is a short python program to calculate the bmi of a person
+# Home-work of dgplug
 """"Update required:-----
-
-    1. this is for adults male, make for adults female and for the children even
+    1. This is for adults male, make for adults female and children even
     2. Add. a BMI chart
     3. Make a GUI version of it
-    
 """
 
-print ("ok")
+print ("Ok")
+
 
 def check(bmi):
-  if bmi<16:
-    print("severe thinness")
-  elif bmi>=16 and bmi<17:
-    print("moderate thinness")
-  elif bmi>=17 and bmi<18.5:
-    print("mild thinness")
-  elif bmi>=18.5 and bmi<25:
-    print("normal")
-  elif bmi>=25 and bmi<30:
-    print("overweight")
-  elif bmi>=30 and bmi<35:
-    print("obese class-1")
-  elif bmi>=35 and bmi<40:
-    print("obese class-2")
-  elif bmi>=40:
-    print("obese class-3")
+    if bmi < 16:
+        print("Severe thinness")
+    elif bmi >= 16 and bmi < 17:
+        print("Moderate thinness")
+    elif bmi >= 17 and bmi < 18.5:
+        print("Mild thinness")
+    elif bmi >= 18.5 and bmi < 25:
+        print("Normal")
+    elif bmi >= 25 and bmi < 30:
+        print("Overweight")
+    elif bmi >= 30 and bmi < 35:
+        print("Obese Class-1")
+    elif bmi >= 35 and bmi < 40:
+        print("Obese Class-2")
+    elif bmi >= 40:
+        print("Obese Class-3")
 
+# print ("we want to calculate your BMI Press 1. to continue")
+ch = int(input("We want to calculate your BMI\nPress 1 to continue\n"))
 
-#print ("we want to calculate your BMI Press 1. to continue")
-ch=int(input("we want to calculate your BMI Press 1. to continue ----- enter your choice====>"))
-
-if ch==1:
-  
-  print ("pressd 1")
-  weight=float(input("Enter your weight in kg"))
-  height=float(input("enter your height in metres"))
-  bmi=weight/(height*height)
-  
-  
-  ch2=int(input("to check your condition that you belong to which category press 1====>"))
-  if ch2==1:
-    print("bmi(kg/m^2)=",bmi)
-    check(bmi)
+if ch == 1:
+    print("Pressed 1")
+    weight = float(input("Enter your weight in kg: "))
+    height = float(input("Enter your height in metres: "))
+    bmi = weight/(height*height)
+    ch2 = int(input("\nTo check your Category\nPress 1\n"))
+    if ch2 == 1:
+        print("BMI: ",bmi)
+        check(bmi)
